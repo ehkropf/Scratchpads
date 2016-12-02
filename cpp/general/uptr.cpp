3fa8a25817a3c42898375870cf6b081b3fbac592
Everett Kropf <ehkropf@gmail.com>
2016-12-02 09:02:45 +0000

    Playing with unique pointer.

---
+++ cpp/general/uptr.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <memory>
+
+/*
+ * What is the best way to store a runtime polymorphic object in a class
+ * when we want the class to manage the lifetime of the object?
+ */
+
+////////////////////////////////////////////////////////////////////////
+/*!
+ * Base.
+ */
+struct Base
+{
+    Base() { std::cout << "Built a Base\n"; }
+    virtual ~Base() { std::cout << "Destroyed a Base\n"; }
+
+    virtual std::string name() const { return "Base"; }
+};
+
+////////////////////////////////////////////////////////////////////////
+/*!
+ * Derived.
+ */
+struct Derived : Base
+{
+    Derived() { std::cout << "Built a Derived\n"; }
+    ~Derived() { std::cout << "Destroyed a Derived\n"; }
+
+    virtual std::string name() const { return "Derived"; }
+};
+
+////////////////////////////////////////////////////////////////////////
+/*!
+ * User of things of Base type.
+ */
+class User
+{
+    std::unique_ptr<Base> _thing;
+
+public:
+    User(Base* thing) : _thing(thing) {}
+
+    friend std::ostream& operator<<(std::ostream& os, const User& user)
+    {
+        os << "User thing claims to be a " << user._thing->name();
+        return os;
+    }
+};
+
+////////////////////////////////////////////////////////////////////////
+int main()
+{
+    Base* b = new Derived;
+    User u(b);
+
+    std::cout << u << std::endl;
+
+    return 0;
+};
