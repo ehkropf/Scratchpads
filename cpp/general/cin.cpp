a10d07da621e79e987e078b5f080fd92958cf038
Everett Kropf <ehkropf@gmail.com>
2017-01-24 19:41:58 +0000

    More practicing.

---
+++ cpp/general/cin.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "input 4 numbers separated by spaces: ";
+    unsigned a, b, c, d;
+    std::cin >> a >> b >> c >> d;
+    std::cout << "\nYou gave " << a << ", " << b << ", " << c << ", " << d << std::endl;
+
+    return 0;
+}
