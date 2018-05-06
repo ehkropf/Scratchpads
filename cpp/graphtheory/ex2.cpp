4f0c528889937813c75d1302a1969978404b7d5f
Everett Kropf <ehkropf@gmail.com>
2018-05-06 06:37:35 +0100

    Finish library problem.

--- cpp/graphtheory/ex2.cpp
+++ cpp/graphtheory/ex2.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
-
-using namespace std;
+#include <queue>
 
 #define COUT(S) std::cout << S << std::endl
 
@@ -22,74 +21,94 @@ struct Edge
 using Edges = std::vector<Edge>;
 using Nodes = std::vector<Node>;
 
-//int roadsAndLibraries(int n, int c_lib, int c_road, vector < vector<int> > cities) {
-int roadsAndLibraries(int n, int c_lib, int c_road, const Edges& E, const Nodes& N)
+void print(int n, int m, int c_lib, int c_road)
 {
-    COUT("n = " << n);
+    COUT("n = " << n << ", m = " << m);
     COUT("c_lib = " << c_lib << ", c_road = " << c_road);
+}
 
-    // Adjacency list:
-    COUT("\nEdges\n========");
-    for (int i = 0; i < E.size(); ++i)
+//int roadsAndLibraries(int n, int c_lib, int c_road, vector < vector<int> > cities) {
+long roadsAndLibraries(int n, int c_lib, int c_road, const Edges& E, Nodes& N)
+{
+    long cost = 0;
+
+    // BFS from each node.
+    std::queue<int> Q;
+    for (int i = 0; i < n; ++i)
     {
-        COUT(E[i].to_node << " " << E[i].next_edge);
+        if (N[i].visited) continue;
+        N[i].visited = true;
+        cost += c_lib;
+        COUT("\nVisited " << i);
+        Q.push(i);
+        while (Q.size())
+        {
+            for (int k = N[Q.front()].last_edge; k != -1; k = E[k].next_edge)
+            {
+                int j = E[k].to_node;
+                if (N[j].visited) continue;
+                N[j].visited = true;
+                cost += c_road;
+                COUT("Visited " << j);
+                Q.push(j);
+            }
+            Q.pop();
+        }
     }
 
-    COUT("\nNodes\n========");
-    for (int i = 0; i < N.size(); ++i)
-    {
-        COUT(N[i].last_edge);
-    }
-
-    return -2;
+    return cost;
 }
 
 int main() {
     int q; // 1 <= q <= 10
-    cin >> q;
+    std::cin >> q;
 
-    for(int a0 = 0; a0 < q; a0++){
-        COUT("+++++=====+++++");
-        int n; // 1 <= n <= 1e5
-        int m; // 0 <= m <= min(1e5, n*(n-1)/2)
-        int c_lib; // 1 <= c_lib <= 1e5
-        int c_road; // 1 <= c_road <= 1e5
-        cin >> n >> m >> c_lib >> c_road;
+    for(int a0 = 0; a0 < q; a0++)
+    {
+        COUT("+++++=== q = " << a0 << " ===+++++");
+        // 1 <= n <= 1e5
+        // 0 <= m <= min(1e5, n*(n-1)/2)
+        // 1 <= c_lib <= 1e5
+        // 1 <= c_road <= 1e5
+        int n, m, c_lib, c_road;
+        std::cin >> n >> m >> c_lib >> c_road;
+        print(n, m, c_lib, c_road);
 
-        if (c_lib < c_road)
+        if (c_lib < c_road || m == 0)
         {
-            cout << n*c_lib << endl;
+            // Could just ignore for speed, but whatever.
+            for (int i = 0; i < m; ++i)
+            {
+                int u, v;
+                std::cin >> u >> v;
+            }
+
+            std::cout << std::endl << long(n)*long(c_lib) << std::endl;
             continue;
         }
 
+        // Input is 1-indexed; graph in memory is 0-indexed.
         Nodes N(n);
         Edges E(2*m);
         for (int i = 0; i < m; ++i)
         {
             int u, v; // 1 <= u,v <= n
-            cin >> u >> v;
+            std::cin >> u >> v;
 
             // Outgoing edge
             E[i].to_node = v-1;
-            E[i].next_edge = N[v-1].last_edge;
-            N[v-1].last_edge = i;
+            E[i].next_edge = N[u-1].last_edge;
+            N[u-1].last_edge = i;
 
             // Incoming edge
             E[m+i].to_node = u-1;
-            E[m+i].next_edge = N[u-1].last_edge;
-            N[u-1].last_edge = m+i;
+            E[m+i].next_edge = N[v-1].last_edge;
+            N[v-1].last_edge = m+i;
         }
 
-        int result = roadsAndLibraries(n, c_lib, c_road, E, N);
+        unsigned result = roadsAndLibraries(n, c_lib, c_road, E, N);
 
-//        vector< vector<int> > cities(m,vector<int>(2));
-//        for(int cities_i = 0;cities_i < m;cities_i++){
-//           for(int cities_j = 0;cities_j < 2;cities_j++){
-//              cin >> cities[cities_i][cities_j];
-//           }
-//        }
-//        int result = roadsAndLibraries(n, c_lib, c_road, cities);
-        cout << result << endl;
+        std::cout << std::endl << result << std::endl;
     }
 
     return 0;
