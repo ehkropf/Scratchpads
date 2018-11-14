6209cc95315b9d02b8facac489fd1d9b38de2856
Everett Kropf <ehkropf@gmail.com>
2018-11-14 12:22:38 +0000

    FFT check with complex and real trig interpolants.

--- cpp/armacheck/fftcheck.cpp
+++ cpp/armacheck/fftcheck.cpp
@@ -8,11 +8,16 @@
 const double pi = std::acos(-1.0);
 const arma::cx_double i2pi(0.0, 2.0*pi);
 
-// Make sample function interesting; put a pole outside the unit circle:
-template <typename ArmaMat>
-ArmaMat testFun(const ArmaMat& zv)
+// Make sample function interesting; put a couple of poles in the unit disk.
+template <typename ArmaCxMat>
+ArmaCxMat testFun(const ArmaCxMat& zv)
 {
-    return zv/(zv - 5.0*std::exp(i2pi/8.0));
+    const arma::cx_vec dv{
+        arma::cx_double(-0.2517, 0.3129), arma::cx_double(0.2307, -0.4667) };
+    ArmaCxMat wv(zv.size(), arma::fill::zeros);
+    for (const auto& d : dv) wv += 1/(zv - d);
+
+    return wv;
 }
 
 ////////////////////////////////////////////////////////////////////////
@@ -26,10 +31,9 @@ ArmaMat testFun(const ArmaMat& zv)
  *
  * using Horner's method.
  */
-template <typename ArmaVec, typename ArmaMat>
-ArmaMat polyval(const ArmaVec& a, const ArmaMat& x)
+arma::cx_vec polyval(const arma::cx_vec& a, const arma::cx_vec& x)
 {
-    ArmaMat pn(arma::size(x), arma::fill::zeros);
+    arma::cx_vec pn(arma::size(x), arma::fill::zeros);
     for (const auto& ai : a) pn = pn%x + ai;
 
     return pn;
@@ -40,16 +44,15 @@ ArmaMat polyval(const ArmaVec& a, const ArmaMat& x)
  *  be extracted. Points to evaluate should of course be on the unit circle.
  *  WARNING: Asumes size of FFT array is even!!!
  */
-template <typename ArmaColVec>
-ArmaColVec trigInterp(const ArmaColVec& c, const ArmaColVec& z)
+arma::cx_vec trigInterp(const arma::cx_vec& c, const arma::cx_vec& z)
 {
     unsigned N = c.n_elem;
 
     const arma::cx_vec& pos_coeff(arma::flipud(c.rows(0, N/2)));
-    ArmaColVec pos_part(polyval(pos_coeff, z));
+    arma::cx_vec pos_part(polyval(pos_coeff, z));
 
-    const arma::cx_vec& neg_coeff(arma::join_vert(c.rows(N/2+1, N-1), ArmaColVec{ 0.0 }));
-    ArmaColVec neg_part(polyval(neg_coeff, ArmaColVec(1/z)));
+    const arma::cx_vec& neg_coeff(arma::join_vert(c.rows(N/2+1, N-1), arma::cx_vec{ 0. }));
+    arma::cx_vec neg_part(polyval(neg_coeff, arma::cx_vec(1/z)));
 
     return pos_part + neg_part;
 }
@@ -58,14 +61,13 @@ ArmaColVec trigInterp(const ArmaColVec& c, const ArmaColVec& z)
  *  Real-valued trig interpolant. Relies on c_k = conj(c_(-k)), and assumes
  *  FFT vector is full and even numbered.
  */
-template <typename ArmaColVec>
-ArmaColVec realTrigInterp(const ArmaColVec& c, const ArmaColVec& z)
+arma::colvec realTrigInterp(const arma::cx_vec& c, const arma::cx_vec& z)
 {
     unsigned N = c.n_elem;
 
-    return c(0) + 2.0*arma::real(
-            polyval(arma::join_vert(arma::flipud(c.rows(1, N/2))), z)
-            );
+    const arma::cx_vec& coeff(arma::join_vert(arma::flipud(c.rows(1, N/2-1)),
+                arma::cx_vec{ 0. }));
+    return c(0).real() + 2.*arma::real(polyval(coeff, z));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -74,11 +76,10 @@ void coeffOut(const arma::cx_vec& c)
 {
     unsigned N = c.n_elem;
 
-    OUT("Coeff (1,5) and (-1,-5)\n...  ("
-            << N/2-5 << ", " << N/2-1 << ") and ("
-            << -int(N/2-5) << ", " << -int(N/2-1) << "): \n"
+    OUT("Coeff (1,5) and (-1,-5):\n"
             << arma::join_horiz(c.rows(1, 5), flipud(c.rows(N-5, N-1)))
-            << "...\n"
+            <<"... (" << N/2-5 << ", " << N/2-1 << ") and ("
+            << -int(N/2-5) << ", " << -int(N/2-1) << "): \n"
             << arma::join_horiz(c.rows(N/2-5, N/2-1), arma::flipud(c.rows(N/2+1, N/2+5)))
             );
 }
@@ -86,11 +87,11 @@ void coeffOut(const arma::cx_vec& c)
 ////////////////////////////////////////////////////////////////////////////////
 int main()
 {
-    // From the unit circle, take the 128-point DFT of a given function.
-    unsigned N = 128;
+    // From the unit circle, take the N-point DFT of a given function.
+    unsigned N = 256;
     arma::cx_vec unit_pts(arma::exp((arma::regspace(0, N-1)/N)*i2pi));
     arma::cx_vec w_pts(testFun(unit_pts));
-    arma::cx_vec c(arma::fft(w_pts));
+    arma::cx_vec c(arma::fft(w_pts)/N);
 
     OUT("Constant (complex): " << c(0));
     OUT("Nyquist: " << c(N/2));
@@ -106,7 +107,7 @@ int main()
             << arma::abs(sample_vals - trigInterp(c, sample_pts)));
 
     // Use the real part of the function only and take the 128-point DFT.
-    arma::cx_vec rc(arma::fft(arma::real(w_pts)));
+    arma::cx_vec rc(arma::fft(arma::real(w_pts))/N);
 
     // For DFT of real function, suppose N is even.
     // Then first coeff is real.
@@ -122,6 +123,8 @@ int main()
     coeffOut(rc);
 
     // Does the trig interpolant return good values at the check points?
+    OUT("Value diff in real interpolant:\n"
+            << arma::abs(arma::real(sample_vals) - realTrigInterp(rc, sample_pts)));
 
     return 0;
 }
